6c7f82c6801d04b57ff19f8353e71b448ec32822
makinj <basicislife@gmail.com>
2014-09-21 08:40:02 -0400

    has basic functionality for submitting and viewing the queue

--- MeNext/src/me/menext/menext/ListParties.java
+++ MeNext/src/me/menext/menext/ListParties.java
@@ -10,87 +10,74 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 public class ListParties extends ActionBarActivity {
 	public static final String PARTYID = null;
-	ListView joinedview;
-	ListView unjoinedview;
-	JSONArray joinedlist;
-	JSONArray unjoinedlist;
+	ListView joinedView;
+	ListView unjoinedView;
+	EditText addPartyView;
+	JSONArray joinedData;
+	JSONArray unjoinedData;
+	String newPartyName;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_list_parties);
-        joinedview = (ListView) findViewById(R.id.joined_party_list);
-        unjoinedview = (ListView) findViewById(R.id.unjoined_party_list);
+        joinedView = (ListView) findViewById(R.id.joined_party_list);
+        unjoinedView = (ListView) findViewById(R.id.unjoined_party_list);
+        addPartyView = (EditText) findViewById(R.id.add_party);
 		new getJoinedList().execute(this);
 		new getUnjoinedList().execute(this);
 		findViewById(R.id.add_party_button_id).setOnClickListener(
+			new View.OnClickListener() {
+				@Override
+				public void onClick(View view) {
+					attemptAddParty();
+				}
+			}
+		);
+		
+		findViewById(R.id.log_out_button).setOnClickListener(
 				new View.OnClickListener() {
 					@Override
 					public void onClick(View view) {
-						
+						attemptLogOut();
 					}
-				});
+				}
+			);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.list_parties, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_list_parties,
-					container, false);
-			return rootView;
-		}
-	}
-
-	public void attemptLogOut(View view) {
-		new logOut().execute(this);
+	public Void attemptAddParty() {
+		newPartyName = addPartyView.getText().toString();
+		new addParty().execute(this);
+		return null;
 	}
 	
-	class getJoinedList extends AsyncTask<Activity, Void, String> {
+	public Void attemptLogOut() {
+		new logOut().execute(this);
+		return null;
+	}
+	
+	public Void attemptGetJoinedList(){
+		new getJoinedList().execute(this);
+		return null;
+	}
+	public Void attemptGetUnjoinedList(){
+		new getUnjoinedList().execute(this);
+		return null;
+	}
+	
+	public class getJoinedList extends AsyncTask<Activity, Void, String> {
 		private Activity activity;
 	    @Override
 	    protected String doInBackground(Activity... activities) {
@@ -103,16 +90,15 @@ public class ListParties extends ActionBarActivity {
 
 	    protected void onPostExecute(String result) {
 	    	try {
-		    	//TextView json_container = (TextView) activity.findViewById(R.id.json_container);
 				JSONObject jObject = new JSONObject(result);
 				String status = jObject.getString("status");
 				if (status.equalsIgnoreCase("success")) {
 					ArrayList<String> parties = new ArrayList<String>();
-					JSONArray joinedlist = jObject.getJSONArray("parties");
-					for(int i = 0, count = joinedlist.length(); i< count; i++)
+					joinedData = jObject.getJSONArray("parties");
+					for(int i = 0, count = joinedData.length(); i< count; i++)
 					{
 					    try {
-					        String party = joinedlist.getJSONObject(i).getString("name");
+					        String party = joinedData.getJSONObject(i).getString("name");
 					        parties.add(party);
 					    }
 					    catch (JSONException e) {
@@ -121,9 +107,9 @@ public class ListParties extends ActionBarActivity {
 					}
 			        ArrayAdapter<String> adapter = new ArrayAdapter<String>(activity,
 			                android.R.layout.simple_list_item_1, android.R.id.text1, parties);
-			        joinedview.setAdapter(adapter);
-	  				final JSONArray partylist = joinedlist;
-			        joinedview.setOnItemClickListener(new OnItemClickListener() {
+			        joinedView.setAdapter(adapter);
+	  				final JSONArray partylist = joinedData;
+			        joinedView.setOnItemClickListener(new OnItemClickListener() {
 		                  @Override
 		                  public void onItemClick(AdapterView<?> parent, View view,
 		                     int position, long id) {
@@ -131,14 +117,11 @@ public class ListParties extends ActionBarActivity {
 		  					try {
 								intent.putExtra(PARTYID, partylist.getJSONObject(position).getString("partyId"));
 							} catch (JSONException e) {
-								// TODO Auto-generated catch block
 								e.printStackTrace();
 							}
 							startActivity(intent);
 		                  }
-		    
 		             }); 
-					//json_container.setText(result);
 				} else {
 					Intent intent = new Intent(activity, LoginActivity.class);
 					startActivity(intent);
@@ -150,8 +133,7 @@ public class ListParties extends ActionBarActivity {
 	   }
 
 	}
-	
-	class getUnjoinedList extends AsyncTask<Activity, Void, String> {
+	public class getUnjoinedList extends AsyncTask<Activity, Void, String> {
 		private Activity activity;
 	    @Override
 	    protected String doInBackground(Activity... activities) {
@@ -164,16 +146,15 @@ public class ListParties extends ActionBarActivity {
 
 	    protected void onPostExecute(String result) {
 	    	try {
-		    	//TextView json_container = (TextView) activity.findViewById(R.id.json_container);
 				JSONObject jObject = new JSONObject(result);
 				String status = jObject.getString("status");
 				if (status.equalsIgnoreCase("success")) {
 					ArrayList<String> parties = new ArrayList<String>();
-					JSONArray unjoinedlist = jObject.getJSONArray("parties");
-					for(int i = 0, count = unjoinedlist.length(); i< count; i++)
+					unjoinedData = jObject.getJSONArray("parties");
+					for(int i = 0, count = unjoinedData.length(); i< count; i++)
 					{
 					    try {
-					        String party = unjoinedlist.getJSONObject(i).getString("name");
+					        String party = unjoinedData.getJSONObject(i).getString("name");
 					        parties.add(party);
 					    }
 					    catch (JSONException e) {
@@ -182,9 +163,9 @@ public class ListParties extends ActionBarActivity {
 					}
 			        ArrayAdapter<String> adapter = new ArrayAdapter<String>(activity,
 			                android.R.layout.simple_list_item_1, android.R.id.text1, parties);
-			        unjoinedview.setAdapter(adapter);
-	  				final JSONArray partylist = unjoinedlist;
-			        unjoinedview.setOnItemClickListener(new OnItemClickListener() {
+			        unjoinedView.setAdapter(adapter);
+	  				final JSONArray partylist = joinedData;
+			        unjoinedView.setOnItemClickListener(new OnItemClickListener() {
 		                  @Override
 		                  public void onItemClick(AdapterView<?> parent, View view,
 		                     int position, long id) {
@@ -192,14 +173,11 @@ public class ListParties extends ActionBarActivity {
 		  					try {
 								intent.putExtra(PARTYID, partylist.getJSONObject(position).getString("partyId"));
 							} catch (JSONException e) {
-								// TODO Auto-generated catch block
 								e.printStackTrace();
 							}
 							startActivity(intent);
 		                  }
-		    
 		             }); 
-					//json_container.setText(result);
 				} else {
 					Intent intent = new Intent(activity, LoginActivity.class);
 					startActivity(intent);
@@ -212,7 +190,7 @@ public class ListParties extends ActionBarActivity {
 
 	}
 	
-	class logOut extends AsyncTask<Activity, Void, Void> {
+	public class logOut extends AsyncTask<Activity, Void, Void> {
 		private Activity activity;
 	    @Override
 	    protected Void doInBackground(Activity... activities) {
@@ -232,23 +210,44 @@ public class ListParties extends ActionBarActivity {
 
 	}
 	
-	class addParty extends AsyncTask<Activity, Void, Void> {
+	public class addParty extends AsyncTask<Activity, Void, String> {
 		private Activity activity;
 	    @Override
-	    protected Void doInBackground(Activity... activities) {
+	    protected String doInBackground(Activity... activities) {
 	    	activity = activities[0];
 	        ServiceHandler sh = new ServiceHandler();
 	        List<NameValuePair> params = new ArrayList<NameValuePair>();
 	        params.add(new BasicNameValuePair("action", "createParty"));
-	        params.add(new BasicNameValuePair("name", "a test party made by the app"));	        
-	        sh.makeServiceCall(activity, "menext", ServiceHandler.POST, params);
-	        return (Void) null;
+	        params.add(new BasicNameValuePair("name", newPartyName));	        
+	        return sh.makeServiceCall(activity, "menext", ServiceHandler.POST, params);
 	    }
 
-	    protected void onPostExecute(Void arg0) {
-			Intent intent = new Intent(activity, LoginActivity.class);
-			startActivity(intent);
-			finish();
+	    protected void onPostExecute(String result) {
+	    	System.out.println(result);
+			JSONObject jObject = null;
+			try {
+				jObject = new JSONObject(result);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			String status = null;
+			try {
+				status = jObject.getString("status");
+			} catch (JSONException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			if (status.equalsIgnoreCase("success")) {
+				Intent intent = new Intent(activity, Party.class);
+				try {
+					intent.putExtra(PARTYID, jObject.getString("partyId"));
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				startActivity(intent);
+			}
 	   }
 
 	}
--- MeNext/src/me/menext/menext/Party.java
+++ MeNext/src/me/menext/menext/Party.java
@@ -1,19 +1,138 @@
 package me.menext.menext;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.support.v7.app.ActionBarActivity;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class Party extends Activity {
+public class Party extends ActionBarActivity {
+	ListView queueView;
+	EditText addVideoView;
+	JSONArray queueData;
+	String newVideoId;
 	String partyId;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_party);
 		Intent intent = getIntent();
 		partyId = intent.getStringExtra(ListParties.PARTYID);
-    	TextView test = (TextView) this.findViewById(R.id.test);
-    	test.setText(partyId);
+		
+        queueView = (ListView) findViewById(R.id.queue_list);
+        addVideoView = (EditText) findViewById(R.id.add_video);
+		new getQueue().execute(this);
+		findViewById(R.id.add_video_button_id).setOnClickListener(
+			new View.OnClickListener() {
+				@Override
+				public void onClick(View view) {
+					attemptAddVideo();
+				}
+			}
+		);
+	}
+	public Void attemptAddVideo() {
+		newVideoId = addVideoView.getText().toString();
+		new addVideo().execute(this);
+		return null;
+	}
+	
+	public Void attemptGetQueue() {
+		new getQueue().execute(this);
+		return null;
+	}
+	
+	public class getQueue extends AsyncTask<Activity, Void, String> {
+		private Activity activity;
+	    @Override
+	    protected String doInBackground(Activity... activities) {
+	    	activity = activities[0];
+	        ServiceHandler sh = new ServiceHandler();
+	        List<NameValuePair> params = new ArrayList<NameValuePair>();
+	        params.add(new BasicNameValuePair("action", "listVideos"));
+	        params.add(new BasicNameValuePair("partyId", partyId));
+	        return sh.makeServiceCall(activity, "menext", ServiceHandler.GET, params);
+	    }
+
+	    protected void onPostExecute(String result) {
+	    	try {
+				JSONObject jObject = new JSONObject(result);
+				String status = jObject.getString("status");
+				if (status.equalsIgnoreCase("success")) {
+					ArrayList<String> videos = new ArrayList<String>();
+					queueData = jObject.getJSONArray("videos");
+					for(int i = 0, count = queueData.length(); i< count; i++)
+					{
+					    try {
+					        String party = queueData.getJSONObject(i).getString("title");
+					        videos.add(party);
+					    }
+					    catch (JSONException e) {
+					        e.printStackTrace();
+					    }
+					}
+			        ArrayAdapter<String> adapter = new ArrayAdapter<String>(activity,
+			                android.R.layout.simple_list_item_1, android.R.id.text1, videos);
+			        queueView.setAdapter(adapter);
+				} else {
+					Intent intent = new Intent(activity, LoginActivity.class);
+					startActivity(intent);
+					finish();
+				}
+	    	} catch (JSONException e) {
+				e.printStackTrace();
+			}
+	   }
+
+	}
+	
+	public class addVideo extends AsyncTask<Activity, Void, String> {
+		private Activity activity;
+	    @Override
+	    protected String doInBackground(Activity... activities) {
+	    	activity = activities[0];
+	        ServiceHandler sh = new ServiceHandler();
+	        List<NameValuePair> params = new ArrayList<NameValuePair>();
+	        params.add(new BasicNameValuePair("action", "addVideo"));
+	        params.add(new BasicNameValuePair("youtubeId", newVideoId));
+	        params.add(new BasicNameValuePair("partyId", partyId));
+	        return sh.makeServiceCall(activity, "menext", ServiceHandler.POST, params);
+	    }
+
+	    protected void onPostExecute(String result) {
+	    	System.out.println(result);
+			JSONObject jObject = null;
+			try {
+				jObject = new JSONObject(result);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			String status = null;
+			try {
+				status = jObject.getString("status");
+			} catch (JSONException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			if (status.equalsIgnoreCase("success")) {
+				attemptGetQueue();
+			}
+	   }
+
 	}
 }
